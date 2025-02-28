87366bd8d18739589bfbb4f993ede9c5906a1a56
Nicky <jnspayload@gmail.com>
2025-02-28 13:05:33 +0530

    first commit

---
+++ lib/bottombutton.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+class Bottom extends StatefulWidget {
+  const Bottom({super.key});
+
+  @override
+  State<Bottom> createState() => _BottomState();
+}
+
+class _BottomState extends State<Bottom> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 60,
+      width: 425,
+      decoration: const BoxDecoration(
+        color: Color(0xFF000000),
+        borderRadius: BorderRadius.only(
+          bottomLeft: Radius.circular(20),
+          bottomRight: Radius.circular(20),
+        ),
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Image.asset(
+            'assets/images/download_removebg_preview_191.png',
+            width: 43,
+            height: 36,
+            fit: BoxFit.contain,
+          ),
+          Image.asset(
+            'assets/images/windows-10.png',
+            width: 43,
+            height: 40,
+            fit: BoxFit.contain,
+          ),
+          Image.asset(
+            'assets/images/download_removebg_preview_201.png',
+            width: 28,
+            height: 28,
+            fit: BoxFit.contain,
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,119 @@
+import 'package:flutter/material.dart';
+import 'package:website/bottombutton.dart';
+import 'package:website/topbutton.dart';
+import 'package:website/wallpapaer.dart';
+import 'package:website/home.dart';
+
+// Custom ScrollPhysics to disable upward scrolling once moved to the second page
+class NoBackScrollPhysics extends ScrollPhysics {
+  const NoBackScrollPhysics({super.parent});
+
+  @override
+  NoBackScrollPhysics applyTo(ScrollPhysics? ancestor) {
+    return NoBackScrollPhysics(parent: buildParent(ancestor));
+  }
+
+  @override
+  bool shouldAcceptUserOffset(ScrollMetrics position) {
+    // Allow scrolling down when not at the bottom
+    if (position.pixels == position.minScrollExtent) {
+      return true;
+    }
+
+    // Prevent scrolling up when not at the top
+    if (position.pixels == position.maxScrollExtent) {
+      return false;
+    }
+
+    return super.shouldAcceptUserOffset(position);
+  }
+
+  @override
+  double applyPhysicsToUserOffset(ScrollMetrics position, double offset) {
+    if (offset > 0) {
+      // Allow scrolling down
+      return offset;
+    } else if (position.pixels <= position.minScrollExtent && offset < 0) {
+      // Prevent scrolling up if at the top of the list (first page)
+      return 0;
+    }
+
+    return super.applyPhysicsToUserOffset(position, offset);
+  }
+}
+
+void main() => runApp(const Lock());
+
+class Lock extends StatefulWidget {
+  const Lock({super.key});
+
+  @override
+  State<Lock> createState() => _LockState();
+}
+
+class _LockState extends State<Lock> {
+  final PageController _pageController = PageController();
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: const PreferredSize(
+          preferredSize: Size(double.infinity, 42), // Set height only
+          child: Center(
+            child: SizedBox(
+              width: 425,
+              child: Topbutton(),
+            ),
+          ),
+        ),
+        body: LayoutBuilder(
+          builder: (context, constraints) {
+            final availableHeight = MediaQuery.of(context).size.height;
+            final appBarHeight = 42.0;
+            final bottomBarHeight = 60.0;
+
+            return Column(
+              children: [
+                Expanded(
+                  child: Center(
+                    child: SizedBox(
+                      width: 425,
+                      height: availableHeight - appBarHeight - bottomBarHeight,
+                      child: PageView(
+                        controller: _pageController,
+                        scrollDirection: Axis.vertical,
+                        physics: const NoBackScrollPhysics(),
+                        children: <Widget>[
+                          Container(
+                            decoration: BoxDecoration(
+                              color: const Color(0xFF0A0A0A),
+                              borderRadius: BorderRadius.circular(20),
+                            ),
+                            child: const Wallpapaer(),
+                          ),
+                          Container(
+                            decoration: BoxDecoration(
+                              color: const Color(0xFF0A0A0A),
+                              borderRadius: BorderRadius.circular(20),
+                            ),
+                            child: const LumiaUI(),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+                Container(
+                  color: Colors.black,
+                  child: const Bottom(),
+                ),
+              ],
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
